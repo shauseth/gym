4511d96dacfe4ecad9e57cc28bc58918588658cf
shaurya <shauryaseth@hotmail.com>
2023-09-29 18:09:52 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -24,14 +24,38 @@ class SarsaLambdaAgent:
         self.last_action = None
         self.last_tiles = None
 
-    def select_action(self):
-        pass
+    def select_action(self, tiles):
+        choice = 'greedy' if np.random.random() > self.epsilon else 'epsilon'
+        action_values = self.w[:, tiles].sum(axis=1)
+        action = action_values.argmax() if choice == 'greedy' else np.random.choice(self.num_actions)
+        return action
 
     def start(self, state):
-        pass
+        tiles = self.tilecoder.get_tiles(state)
+        action = self.select_action(tiles)
+        self.z *= 0.0
+        self.last_action = action
+        self.last_tiles = tiles.copy()
+        return self.last_action
 
     def step(self, state, reward):
-        pass
+        tiles = self.tilecoder.get_tiles(state)
+        action = self.select_action(tiles)
+        delta = reward
+        for i in self.last_tiles:
+            delta -= self.w[self.last_action][i]
+            self.z[self.last_action][i] = 1
+        for i in tiles:
+            delta += self.gamma * self.w[action][i]
+        self.w += self.alpha * delta * self.z
+        self.z *= self.gamma * self.lambd
+        self.last_action = action
+        self.last_tiles = tiles.copy()
+        return self.last_action
 
     def end(self, reward):
-        pass
+        delta = reward
+        for i in self.last_tiles:
+            delta -= self.w[self.last_action][i]
+            self.z[self.last_action][i] = 1
+        self.w += self.alpha * delta * self.z
